0b0c4f930e67b131cbfcf28080f4dfa942d6a769
feite-64 <ad1793031817@163.com>
2022-08-11 15:28:55 +0800

    测试

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const div = ref(null)
+onMounted(() => {
+  console.log(div.value);
+  })
+  
+</script>
+
+<template>
+  <div ref="div"></div>
+</template>
+
+<style scoped>
+</style>
