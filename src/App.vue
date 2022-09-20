fae807f8f543a75e2cdef546d56e060ff1540522
feite-64 <ad1793031817@163.com>
2022-09-20 16:28:12 +0800

    -a

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import HelloWord from './components/HelloWorld.vue'
+import { ref, watch } from 'vue';
 
-const div = ref(null)
-onMounted(() => {
-  console.log(div.value);
-  })
-  
+let students = ref(1)
+setTimeout(() => {
+  students.value = 2
+}, 1000);
+
+watch(() => students.value, (newVal, oldVal) => {
+  console.log(newVal, oldVal);
+})
 </script>
 
-<template>
-  <div ref="div"></div>
+<template >
+  <div v-if="students">{{students}}</div>
+  <HelloWord v-if="students!=1" :students="students"></HelloWord>
 </template>
 
-<style scoped>
+
+<style scoped lang="scss">
+li {
+  width: 200px;
+}
+
+.el {
+  height: calc(100vh - 60px);
+  background-color: #5f89ce;
+}
+
+// 定义数组，数组元素用逗号隔开
+$liColor: #f5ad1b, #5f89ce, #94bf45, #da8ec5, #78bfc2, #bec278;
+
+// 开始 @each 循环遍历数组
+// $c 作为循环变量，代表了数组的元素，不是索引~！！！
+@each $c in $liColor {
+  $i: index($liColor, $c); // 获取 $c 在数组中的索引，并赋值给 $i 赋值用冒号，不是等号~！
+
+  li:nth-child(#{$i}) {
+    // 经典的地方来了，SCSS 循环是从 1 开始，不是 0 哦~
+    background: $c; // 背景色
+
+    &:hover {
+      background: lighten($c, 10%); // hover 后的颜色
+    }
+  }
+}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        //重定向
+        target: 'http://localhost:3000', //要跳转的位置 ,拼接
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') //重写api，替换成空的
+      }
+    }
+  }
 })
